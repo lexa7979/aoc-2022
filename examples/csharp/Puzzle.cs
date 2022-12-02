13b672dbb8bddb878bf89127c742af6cc0193433
Alexander Urban <alexander.urban@svenskaspel.se>
2022-12-02 07:04:42 +0100

    Removing dummy folders and examples

--- examples/csharp/Puzzle.cs
+++
@@ -1,8 +0,0 @@
-namespace aoc;
-
-public static class Puzzle
-{
-    public static int GetSolutionPart1(IEnumerable<int> input) => input.Sum();
-
-    public static int GetSolutionPart2(IEnumerable<int> input) => input.Aggregate((i, j) => i * j);
-}
